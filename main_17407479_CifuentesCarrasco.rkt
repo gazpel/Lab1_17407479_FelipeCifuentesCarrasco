d921ae653ccb8746dfc2bf114eb020dc5cca9f1c
Felipe Cifuentes <felipe.cifuentes0@gmail.com>
2026-05-18 14:34:28 -0400

    Archivo principal (require/provide)

---
+++ main_17407479_CifuentesCarrasco.rkt
@@ -0,0 +1,23 @@
+#lang racket
+;; ============================================================
+;; Archivo Main: require/provide a todos los archivos
+;; ============================================================
+(require "base_17407479_CifuentesCarrasco.rkt")
+(require "TDAataque_17407479_CifuentesCarrasco.rkt")
+(require "TDAcarta_17407479_CifuentesCarrasco.rkt")
+(require "TDAmazo_17407479_CifuentesCarrasco.rkt")
+(require "TDApokemonEnJuego_17407479_CifuentesCarrasco.rkt")
+(require "TDAjugador_17407479_CifuentesCarrasco.rkt")
+(require "TDAjuego_17407479_CifuentesCarrasco.rkt")
+(require "printGame_17407479_CifuentesCarrasco.rkt")
+(require "funciones_17407479_CifuentesCarrasco.rkt")
+
+(provide (all-from-out "base_17407479_CifuentesCarrasco.rkt"))
+(provide (all-from-out "TDAataque_17407479_CifuentesCarrasco.rkt"))
+(provide (all-from-out "TDAcarta_17407479_CifuentesCarrasco.rkt"))
+(provide (all-from-out "TDAmazo_17407479_CifuentesCarrasco.rkt"))
+(provide (all-from-out "TDApokemonEnJuego_17407479_CifuentesCarrasco.rkt"))
+(provide (all-from-out "TDAjugador_17407479_CifuentesCarrasco.rkt"))
+(provide (all-from-out "TDAjuego_17407479_CifuentesCarrasco.rkt"))
+(provide (all-from-out "printGame_17407479_CifuentesCarrasco.rkt"))
+(provide (all-from-out "funciones_17407479_CifuentesCarrasco.rkt"))
